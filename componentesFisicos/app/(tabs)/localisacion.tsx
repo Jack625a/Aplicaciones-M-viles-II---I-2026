f2a3e91417a434f927cd50dae87f3c7e2b66eeba
Jack625a <ktfut15@gmail.com>
2026-04-17 17:14:45 -0400

    Mapas con sensor de gps

--- componentesFisicos/app/(tabs)/localisacion.tsx
+++ componentesFisicos/app/(tabs)/localisacion.tsx
@@ -2,11 +2,33 @@
 import React, {useState,useEffect } from "react";
 import {Text,View,StyleSheet,Button,Alert} from "react-native"
 import * as Location from 'expo-location'
+//Importacion de los mapas
+import MapView, {Marker} from 'react-native-maps';
 
 export default function gpsPantalla(){
     //Paso 2. Gestion de estados
     const [localisacion,setLocalisacion]=useState(null);
     const [mensajeError,setMensajeError]=useState(null);
+
+    useEffect(()=>{
+        (async()=>{
+             //Pedir los permisos
+            let {status}=await Location.requestForegroundPermissionsAsync();
+
+            if (status!== 'granted'){
+                setMensajeError('Permiso de ubicacion denegado...');
+                alert("Permisos denegados...");
+                return;
+            }
+            //Obtener la latitud y longitud 
+            let ubicacionActual=await Location.getCurrentPositionAsync({
+                accuracy:Location.Accuracy.Balanced,
+            });
+            setLocalisacion(ubicacionActual);
+            }
+
+        )();
+    },[]);
 
     //Paso3. Funcion para obtener la ubicacion
     const obtenerUbicacion= async()=>{
@@ -37,6 +59,31 @@ export default function gpsPantalla(){
             )
         }
         <Button title="Obtener Ubicacion" onPress={obtenerUbicacion}></Button>
+        <MapView style={styles.mapa}
+            initialRegion={{
+                latitude:localisacion.coords.latitude,
+                longitude:localisacion.coords.longitude,
+                latitudeDelta:0.01,
+                longitudeDelta:0.01
+            }}
+        >
+            <Marker
+                coordinate={{
+                    latitude:localisacion.coords.latitude,
+                    longitude:localisacion.coords.longitude
+                }}
+                title="Mi Ubicacion Actual"
+                description="Prueba Ubicacion...."
+            />
+
+        </MapView>
+
         </View>
     );
 }
+const styles= StyleSheet.create({
+    mapa:{
+        width:"100%",
+        height:"100%"
+    }
+})
